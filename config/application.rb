34bbabf82328a9c0e8fe22541fe35d03f700848d
cesar ortiz <ceortiz98@hotmail.com>
2018-08-05 19:19:52 -0400

    enabled cors

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,11 @@ module NewTuaula
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.action_dispatch.default_headers = {
+      'Access-Control-Allow-Origin' => '*',
+      'Access-Control-Request-Method' => %w{GET POST PUT DELETE OPTIONS}.join(",")
+    }
+
   end
 end
